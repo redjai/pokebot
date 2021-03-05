891cfae69e026bedcc7fa9876ce3e2a54b3bc6c8
stephen creedon <s.creedon@livelinktechnology.net>
2021-03-05 21:54:02 +0000

    tidied user spec

--- bot/spec/handlers/user_spec.rb
+++ bot/spec/handlers/user_spec.rb
@@ -4,7 +4,8 @@ require 'bot/event_builders'
 
 describe 'handler' do
 
-  let(:bot_event){ build(:bot_event, bot_event_record: Bot::EventBuilders.favourite_new(source: :interactions, favourite_recipe_id: '12345')) }
+  let(:bot_event_record){ Bot::EventBuilders.favourite_new(source: :interactions, favourite_recipe_id: '12345') }
+  let(:bot_event){ build(:bot_event, bot_event_record: bot_event_record) }
   let(:aws_event){ build(:aws_event, bot_event: bot_event) }
   let(:context){ {} }
 

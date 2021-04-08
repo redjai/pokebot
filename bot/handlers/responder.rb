be59279b8c16d5b1bbc0c704f710066515eabca7
stephen creedon <s.creedon@livelinktechnology.net>
2021-04-08 17:48:53 +0100

    removed redundant events constant

--- bot/handlers/responder.rb
+++ bot/handlers/responder.rb
@@ -3,14 +3,6 @@ require 'topic/topic'
 
 module Responder
   class Handler
-    EVENTS = [
-               Topic::Recipes::FOUND, 
-               Topic::Messages::RECEIVED, 
-               Topic::Users::FAVOURITES_UPDATED,
-               Topic::Users::ACCOUNT_UPDATED,
-               Topic::Users::ACCOUNT_READ
-             ]
-
     def self.handle(event:, context:)
       Lambda::Event.process_sqs(aws_event: event, controller: :responder, accept: { 
         recipes: %w{ found },

5ad2f599af7fabb160c6628bcc4f5d042af0ffd6
stephen creedon <s.creedon@livelinktechnology.net>
2021-08-01 12:36:52 +0100

    updated durations table

--- bot/storage/kanbanize/postgres/column_durations.rb
+++ bot/storage/kanbanize/postgres/column_durations.rb
@@ -3,14 +3,18 @@ require 'storage/kanbanize/s3/task'
 module Storage
   module Postgres
     class ColumnDurations
-      def initialize(client_id:, board_id:, history_details:)
+      def initialize(client_id:, board_id:, task_id:, workflow:, history_details:)
         @client_id = client_id
         @board_id = board_id
+        @task_id = task_id
+        @workflow = workflow
         @history_details = history_details
       end
 
       def movements
-        @movements ||= @history_details.select{|history_detail| history_detail.history_event == 'Task moved'  && history_detail.details != "The task was reordered within the same board cell." }
+        @movements ||= @history_details.select do |history_detail| 
+          history_detail.moved_column? || history_detail.created? || history_detail.archived?
+        end.sort_by(&:id)
       end
 
       def resource
@@ -18,9 +22,12 @@ module Storage
       end
 
       def store!
+        execute(delete_history(@task_id))
         movements.each do |movement|
-          execute(history_entry_sql(movement))
-          #execute(history_exit_sql(movement)) 
+          execute(history_created_sql(movement)) if movement.created?
+          execute(history_entry_sql(movement))  if movement.moved_column?
+          execute(history_exit_sql(movement)) if movement.moved_column?
+          execute(history_archived_sql(movement))  if movement.archived?
         end
       end
 
@@ -33,20 +40,68 @@ module Storage
         })
       end
 
-      def column_name(task_id, column_name)
-        "#{@client_id}:#{@board_id}:#{task_id}:#{column_name}"
+      def task
+        "#{@client_id}:#{@board_id}:#{@task_id}"
+      end
+
+      def delete_history(history_detail)
+        %Q{
+          DELETE FROM
+            public.column_durations
+          WHERE
+            task = '#{task}'
+        }
+      end
+
+      def history_created_sql(history_detail)
+        %Q{
+           INSERT INTO 
+             public.column_durations(
+              task,
+              workflow,
+              board_id,
+              column_name,
+              entry_history_id,
+              entry_details,
+              entry_event_type,
+              entry_history_event,
+              entry_at
+           ) VALUES (
+              '#{task}',
+              '#{@workflow}',
+              '#{@board_id}',
+              'UNKNOWN - CREATED',
+              '#{history_detail.id}',
+              '#{history_detail.details_sql_safe}',
+              '#{history_detail.event_type}',
+              '#{history_detail.history_event}',
+              '#{history_detail.timestamp}'
+           ) ON CONFLICT DO NOTHING
+        }
       end
 
       def history_entry_sql(history_detail)
         %Q{
            INSERT INTO 
              public.column_durations(
+              task,
+              workflow,
+              board_id,
               column_name,
               entry_history_id,
+              entry_details,
+              entry_event_type,
+              entry_history_event,
               entry_at
            ) VALUES (
-              '#{column_name(history_detail.task_id, history_detail[:to])}',
+              '#{task}',
+              '#{@workflow}',
+              '#{@board_id}',
+              '#{history_detail[:to]}',
               '#{history_detail.id}',
+              '#{history_detail.details_sql_safe}',
+              '#{history_detail.event_type}',
+              '#{history_detail.history_event}',
               '#{history_detail.timestamp}'
            ) ON CONFLICT DO NOTHING
         }
@@ -55,14 +110,42 @@ module Storage
       def history_exit_sql(history_detail)
         %Q{
            UPDATE  
-             public.column_durations
-           SET exit_history_id = #{history_detail.id}, exit_at = #{history_detail.timestamp}
+              public.column_durations
+           SET 
+              exit_history_id = #{history_detail.id}, 
+              exit_at = '#{history_detail.timestamp}', 
+              exit_details = '#{history_detail.details_sql_safe}',
+              exit_event_type = '#{history_detail.event_type}',
+              exit_history_event = '#{history_detail.history_event}'
            FROM (
              SELECT entry_history_id FROM public.column_durations
-             WHERE column_name = '#{column_name(history_detail.task_id, history_detail[:from])}'
-             AND exit_history_id IS NULL ORDER BY entry_at ASC
+             WHERE task = '#{task}'
+             AND column_name = '#{history_detail[:from]}'
+             AND exit_history_id IS NULL 
+             AND entry_history_id < #{history_detail.id}
+             ORDER BY entry_history_id ASC
              LIMIT 1
-           ) AS subquery WHERE entry_history_id = subquery.entry_history_id returning entry_history_id
+           ) AS subquery WHERE public.column_durations.entry_history_id = subquery.entry_history_id returning subquery.entry_history_id ;
+        }
+      end
+
+      def history_archived_sql(history_detail)
+        %Q{
+           UPDATE  
+              public.column_durations
+           SET 
+              exit_history_id = #{history_detail.id}, 
+              exit_at = '#{history_detail.timestamp}', 
+              exit_details = '#{history_detail.details_sql_safe}',
+              exit_event_type = '#{history_detail.event_type}',
+              exit_history_event = '#{history_detail.history_event}'
+           FROM (
+             SELECT entry_history_id FROM public.column_durations
+             WHERE task = '#{task}'
+             AND exit_history_id IS NULL 
+             AND entry_history_id < #{history_detail.id}
+             LIMIT 1
+           ) AS subquery WHERE public.column_durations.entry_history_id = subquery.entry_history_id returning subquery.entry_history_id ;
         }
       end
     end
--- bot/storage/kanbanize/postgres/migrations/1_create_histories_table.rb
+++ bot/storage/kanbanize/postgres/migrations/1_create_histories_table.rb
@@ -3,20 +3,30 @@ module Migrations
     def up
       %q{
           CREATE TABLE public.column_durations (
+              task varchar(75),
+              board_id integer,
+              workflow integer,
               column_name varchar(75),
+              entry_details text,
               entry_history_id integer,
+              entry_history_event varchar(50),
+              entry_event_type varchar(50),
+              exit_details text,
               exit_history_id integer,
+              exit_history_event varchar(50),
+              exit_event_type varchar(50),
               entry_at timestamp without time zone,
-              exit_at timestamp without time zone
+              exit_at timestamp without time zone,
+              duration integer
           );
-          CREATE INDEX column_name_idx ON public.column_durations USING BTREE (column_name);
+          CREATE INDEX column_name_task_idx ON public.column_durations USING BTREE (task, column_name);
           CREATE UNIQUE INDEX entry_history_id_idx ON public.column_durations USING BTREE (entry_history_id);
       }
     end
 
     def down
       %q{ 
-          DROP INDEX column_name_idx;
+          DROP INDEX column_name_task_idx;
           DROP INDEX entry_history_id_idx;
           DROP TABLE public.column_durations;
         } 
